3e07849e32cb1cda460056ad11874069b32a4b00
Alex Schneider <alex@schneids.me>
2015-09-20 19:57:52 -0700

    Add working tests

--- Leaderboard/test/test_webserver.js
+++ Leaderboard/test/test_webserver.js
@@ -7,10 +7,19 @@ const htmlFolder = path.resolve(__dirname, 'html');
 
 const equalsHtml = fileName => fs.readFileSync(path.resolve(htmlFolder, fileName), 'utf8');
 
-describe('GET /', () => {
-  it('should return the index page', done => {
-    request(app)
-      .get('/')
-      .expect(equalsHtml('index.html'), done);
+describe('GET', () => {
+  describe('/', () => {
+    it('should return the index page', done => {
+      request(app)
+        .get('/')
+        .expect(equalsHtml('index.html'), done);
+    });
+  });
+  describe('/users/register', () => {
+    it('should return the registration page', done => {
+      request(app)
+        .get('/users/register')
+        .expect(equalsHtml('register.html'), done);
+    });
   });
 });
