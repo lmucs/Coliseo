c7e2b540e86b5733ead062cde264f5f63af64470
Andrew Akers <andrew@akrs.me>
2015-10-01 18:51:08 -0700

    mayeb abandon in place?

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -6,12 +6,15 @@ public class Player : Actor
 {
 	public float speed = 6f;
 	public float movementSpeed = 10f;
+	private float cameraRotationX = 0f;
+	private Transform cameraTransform;
 
 	PlayerControls controller;
 	// Use this for initialization
 	void Start ()
 	{
         controller = new PlayerControls(this);
+		cameraTransform = transform.Find("ScientistSkeleton/Hips/Spine/Spine1/Neck/Head/Camera");
 	}
 	
 	// Update is called once per frame
@@ -22,24 +25,22 @@ public class Player : Actor
 
 	public override void Move(float h, float v)
 	{
-		// Set the movement vector based on the axis input.
-		movement.Set(h, 0f, v);
-		
-		// Normalise the movement vector and make it proportional to the speed per second.
-		movement = movement.normalized * speed * Time.deltaTime;
-
-        // Move the player to it's current position plus the movement.
-        rigidbody.MovePosition(transform.position + transform.rotation * movement);
+		Animating (h, v, 0);
 	}
 
-	public void Turn (float x, float y)
+	// Now that the camera is directly on the head, we can, for the time being, 
+	// have the controller directly move the head to look down. For now.
+	public void Turn(float x, float y)
 	{
-
-//		Debug.Log ("turnX: " + x + ", turnY: " + y);
-		Vector3 vec = new Vector3(y, x, 0f);
+		Vector3 vec = new Vector3(0, x, 0);
 		Quaternion deltaRotation = Quaternion.Euler(vec * Time.deltaTime + transform.rotation.eulerAngles);
-
-        rigidbody.AddRelativeTorque(vec * rigidbody.mass / 2f);
-        rigidbody.MoveRotation(deltaRotation);
+		
+		if (!UnityEngine.VR.VRDevice.isPresent)
+		{
+			cameraRotationX = Mathf.Clamp(cameraRotationX + y, -90, 90);
+			cameraTransform.localEulerAngles = new Vector3(cameraRotationX, 0, 0);
+		}
+		
+		Animating (0, 0, a);
 	}
 }
