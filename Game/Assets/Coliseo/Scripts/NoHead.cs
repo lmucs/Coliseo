dab299260a2f77324ddd742922ec163ec9a7a4f6
Albert Johnston <ajohn104@lion.lmu.edu>
2015-10-13 12:56:05 -0700

    Updated to final name; changed initial player health display

---
+++ Game/Assets/Coliseo/Scripts/NoHead.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoHead : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	void Start () {
+        Transform transform = gameObject.GetComponent<Transform>();
+        Transform headTransform = transform.FindChild("Head");
+        transform.localScale = new Vector3(1.0F, 0.1F, 1.0F);
+        headTransform.localScale = new Vector3(0.1F, 0.1F, 0.1F);
+
+
+        Transform cameraRigTransform = headTransform.FindChild("CameraRig");
+        cameraRigTransform.localPosition = new Vector3(2.0F, 13.0F, 2.0F); // comment this out to place head correctly.
+    }
+}
