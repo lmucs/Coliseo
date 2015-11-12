5393b284713fe58a0391232832652eeb68af5747
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-11-12 10:44:27 -0800

    Added game over and level start UI

---
+++ Game/Assets/pause menu/Start/UIManager.cs
@@ -0,0 +1,45 @@
+﻿//using UnityEngine;
+//using UnityEngine.UI;
+//using System.Collections;
+//
+//public class UIManager : MonoBehaviour {
+//
+//	public GameObject pausePanel;
+//	public bool isPaused;
+//	// Use this for initialization
+//	void Start () {
+//		isPaused = false;
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		if (isPaused) {
+//			PauseGame (true);
+//		} else {
+//			PauseGame (false);
+//		}
+//
+//		if (Input.GetButtonDown ("start") || Input.GetButtonDown("desktop_start")) {
+//			switchPause ();
+//		}
+//	
+//	}
+//
+//	void PauseGame(bool state){
+//		if (state) {
+//			pausePanel.SetActive (true);
+//			Time.timeScale = 0.0f;
+//		} else {
+//			Time.timeScale = 1.0f;
+//			pausePanel.SetActive(false);
+//		}
+//	}
+//
+//	public void switchPause(){
+//		if (isPaused) {
+//			isPaused = false;
+//		} else {
+//			isPaused = true;
+//		}
+//	}
+//}
