7914ece6f9444d60284686d0ce7fb1bf89ed14d0
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-11-24 03:32:50 -0800

    Added pause game functionality

--- Game/Assets/pause menu/Start/UIManager.cs
+++ Game/Assets/pause menu/Start/UIManager.cs
@@ -1,45 +1,45 @@
-﻿//using UnityEngine;
-//using UnityEngine.UI;
-//using System.Collections;
-//
-//public class UIManager : MonoBehaviour {
-//
-//	public GameObject pausePanel;
-//	public bool isPaused;
-//	// Use this for initialization
-//	void Start () {
-//		isPaused = false;
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//		if (isPaused) {
-//			PauseGame (true);
-//		} else {
-//			PauseGame (false);
-//		}
-//
-//		if (Input.GetButtonDown ("start") || Input.GetButtonDown("desktop_start")) {
-//			switchPause ();
-//		}
-//	
-//	}
-//
-//	void PauseGame(bool state){
-//		if (state) {
-//			pausePanel.SetActive (true);
-//			Time.timeScale = 0.0f;
-//		} else {
-//			Time.timeScale = 1.0f;
-//			pausePanel.SetActive(false);
-//		}
-//	}
-//
-//	public void switchPause(){
-//		if (isPaused) {
-//			isPaused = false;
-//		} else {
-//			isPaused = true;
-//		}
-//	}
-//}
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject pausePanel;
+	public bool isPaused;
+	// Use this for initialization
+	void Start () {
+		isPaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			PauseGame (true);
+		} else {
+			PauseGame (false);
+		}
+
+		if (Input.GetButtonDown ("Start") || Input.GetButtonDown("Desktop_Start")|| Input.GetButtonDown("Cancel")) {
+			switchPause ();
+		}
+	
+	}
+
+	void PauseGame(bool state){
+		if (state) {
+			pausePanel.SetActive (true);
+			Time.timeScale = 0.0f;
+		} else {
+			Time.timeScale = 1.0f;
+			pausePanel.SetActive(false);
+		}
+	}
+
+	public void switchPause(){
+		if (isPaused) {
+			isPaused = false;
+		} else {
+			isPaused = true;
+		}
+	}
+}
