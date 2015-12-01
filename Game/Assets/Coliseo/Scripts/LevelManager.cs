a8dd36e499b4e0e0c88d5d96acf9c2bd5df41b3b
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-12-01 10:35:32 -0800

    Made a Start Menu
    added a start menu that begins the game when the play button is pressed.
    If the game is run from a build the exit button will exit out the game.
    And the options button is just for show.

---
+++ Game/Assets/Coliseo/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadScene (string name) {
+		Application.LoadLevel (name);
+	}
+	
+	// Update is called once per frame
+	public void QuitGame () {
+		Application.Quit ();
+	}
+}
