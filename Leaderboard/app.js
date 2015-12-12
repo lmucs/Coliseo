d73206eb71a32abbf691ec2fe96d76503a7ead8a
Alex Schneider <alex@schneids.me>
2015-12-12 13:11:34 -0800

    Add favicon and minor fixes

--- Leaderboard/app.js
+++ Leaderboard/app.js
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 setupSession(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
