87f811e1d39a0edba77023ecd40bc260353371e3
Alex Schneider <alex@schneids.me>
2015-12-13 00:55:08 -0800

    Minor leaderboard improvements and initial version of API

--- Leaderboard/routes/api/index.js
+++ Leaderboard/routes/api/index.js
@@ -1,25 +1,39 @@
 import express from 'express';
 
 import v1 from './v1';
-import {User, Score} from '../../database';
-import {asyncWrap} from '....//helper';
 
 const router = express.Router();
+router.use('/v1', v1);
 
-// gethighscoreforuser
-// postscore
+// We want to have separate error handling because we're using JSON here.
 
-router.get('/user/:username'); //user info
+router.use((req, res, next) => {
+  const err = new Error(`Not Found: ${req.originalUrl}`);
+  err.status = 404;
+  next(err);
+});
 
-const getScores = async (req, res, next) => {
-  if (req.params.username !== null) {
-    const user = await User.findOne({
-      where: {
-        username: req.params.usernamePattern.toLowerCase(),
-      },
+if (router.get('env') === 'development') {
+  router.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err,
     });
-  }
-};
-router.get('/scores/:username?', asyncWrap(getScores));
+    console.error(err.stack);
+  });
+}
 
-router.post('/scores/:username');
+router.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  let error = {};
+  if (req.app.get('env') === 'development') {
+    error = err;
+  }
+  res.json({
+    message: err.message,
+    error,
+  });
+});
+
+export default router;
