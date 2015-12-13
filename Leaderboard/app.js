5e4d9c65391ba13869848bb939386c2efa5c42ed
Alex Schneider <alex@schneids.me>
2015-12-12 19:05:41 -0800

    Add support for leaderboard and scoring in the database and app logic

--- Leaderboard/app.js
+++ Leaderboard/app.js
@@ -4,14 +4,13 @@ import favicon from 'serve-favicon';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import hbs from 'hbs';
+import _ from 'lodash';
 
 import routes from './routes';
-import users from './routes/users';
-import leaderboard from './routes/leaderboard';
 import setupLocals from './locals'; // Populate our app with custom locals
 import secret from './secret';
 import setupSession from './session';
-import {User} from './database';
+import {User, Score} from './database';
 import {calculateSaltHash} from './cryptography';
 
 const app = express();
@@ -32,12 +31,10 @@ app.use(cookieParser(secret));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
-app.use('/leaderboard', leaderboard);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error(`Not Found: ${req.originalUrl}`);
   err.status = 404;
   next(err);
 });
@@ -53,13 +50,34 @@ if (app.get('env') === 'development') {
       message: err.message,
       error: err,
     });
+    console.error(err.stack);
   });
   // Setup test user and password
-  User.create({
-    username: 'test',
-    email: 'example@example.com',
-    password: calculateSaltHash('test'),
-  });
+  (async () => {
+    const testUser = await User.create({
+      username: 'test',
+      email: 'example@example.com',
+      password: calculateSaltHash('test'),
+      biography: `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra ex
+consequat velit finibus dapibus. Morbi vulputate sapien et purus placerat,
+eget tristique urna hendrerit. Mauris et orci ullamcorper, porta enim
+quis, consectetur dolor. Sed ullamcorper, ligula vel maximus feugiat, ex
+turpis facilisis risus, eu commodo lacus risus id ante. Curabitur bibendum
+ante lorem, nec ullamcorper nisi placerat ac. Aliquam erat volutpat.
+Praesent facilisis, lorem eget accumsan convallis, tortor diam cursus
+orci, eu semper tortor odio id arcu. Pellentesque non nibh elit. Vivamus
+eget arcu lorem. Sed sit amet interdum sapien.
+`.trim(),
+
+    });
+    const scores = [];
+    for (let i = 0; i < 100; i++) {
+      scores.push({score: _.random(9000)});
+    }
+    await Score.bulkCreate(scores);
+    testUser.addScores(await Score.findAll());
+  })();
 }
 
 // production error handler
