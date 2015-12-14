ca01b8ca88f128ae523ac0fd480a7810f6762cf9
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-12-13 19:37:22 -0800

    Made outline for Leaderboard

--- Game/Assets/Coliseo/Scripts/LevelManager.cs
+++ Game/Assets/Coliseo/Scripts/LevelManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
+	public GameObject Scoreboard;
 
 	// Use this for initialization
 	public void LoadScene (string name) {
@@ -11,5 +12,13 @@ public class LevelManager : MonoBehaviour {
 	// Update is called once per frame
 	public void QuitGame () {
 		Application.Quit ();
+	}
+
+	public void HighScores (){
+		Scoreboard.SetActive (true);
+	}
+
+	public void BackToMainMenu (){
+		Scoreboard.SetActive (false);
 	}
 }
